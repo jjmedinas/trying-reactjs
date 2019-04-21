d59c5b998d4b9fda71a59d51606be0c5e1ec5b2b
jjmedinas <juan.ms@outlook.com>
2019-04-21 17:20:09 -0500

    Add functional stateless components

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -5,31 +5,48 @@ class ContactsList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ''
+            search: '',
+            contacts: props.contacts
         };
     }
 
 
-    updateSearch(event){
+    updateSearch(event) {
         this.setState({search: event.target.value});
+    }
+
+    addContact(event) {
+        event.preventDefault();
+        let name = this.refs.name.value;
+        let phone = this.refs.phone.value;
+        let id = Math.floor((Math.random() * 100) +1);
+        this.setState({
+            contacts: this.state.contacts.concat({id, name, phone})
+        });
+
+        this.refs.name.value = '';
+        this.refs.phone.value= '';
+
     }
 
     render() {
 
-        let filteredContacts = this.props.contacts.filter(
+        let filteredContacts = this.state.contacts.filter(
             (contact) => {
-                // console.log("search state " + this.state.search)
-                // console.log("contact name " + contact.name)
-                // console.log("Result " + contact.name.indexOf(this.state.search))
-
                 return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
             }
         );
         return (
             <div>
                 <input type="text"
+                    placeholder="Search"
                     value={this.state.search}
                     onChange={this.updateSearch.bind(this)}/>
+                <form onSubmit={this.addContact.bind(this)}>
+                    <input type="text" ref="name"/>
+                    <input type="text" ref="phone"/>
+                    <button type="submit">Add New Contact</button>
+                </form>
                 <ul>
                     {filteredContacts.map((contact) => {
                         return <Contact contact={contact}
